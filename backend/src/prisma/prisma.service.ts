6516452afc0afaa0172bcc4a8091f21b31b6bfb1
jeffry-Salazar <jeffryalex14@gmail.com>
2026-06-25 22:05:24 -0500

    pruebas en thunderclient de BD

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,8 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config'; // Fuerza la lectura de tu archivo .env
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    // 1. Configuramos el pool de conexiones usando tu DATABASE_URL
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    
+    // 2. Creamos el adaptador oficial de Postgres
+    const adapter = new PrismaPg(pool);
+    
+    // 3. Inicializamos PrismaClient pasando el adaptador (¡Esto soluciona el error!)
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }

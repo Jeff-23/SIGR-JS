be2bb1c8055a43c2b36f81b88b4a188aece28a86
jeffry-Salazar <jeffryalex14@gmail.com>
2026-06-26 22:38:19 -0500

    instlaciones de seguridad para phishing y fuerza bruta

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsuariosModule } from './modulos/usuarios/usuarios.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsuariosModule } from './modulos/usuarios/usuarios.module';
+import { AuthModule } from './modulos/auth/auth.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './modulos/auth/auth.module';
 
 @Module({
-  imports: [UsuariosModule, PrismaModule],
+  imports: [
+    // Escudo activado: Máximo 10 peticiones cada 60 segundos (60000 milisegundos) por IP
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+    PrismaModule, 
+    UsuariosModule, 
+    AuthModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // Esto hace que el escudo proteja TODO el sistema automáticamente
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    }
+  ],
 })
 export class AppModule {}
